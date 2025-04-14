5190bd0fcb3e89beaf8c2549a99806dc8ac487e5
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-04-14 14:48:07 +0000

    Praticando pra melhorar !

---
+++ Aula-01/Exercicio2.py
@@ -0,0 +1,12 @@
+print("Digite o primeiro valor:")
+valor1 = float(input())
+
+print("Digite o segundo valor:")
+valor2 = float(input())
+
+soma = valor1 + valor2
+
+print(f"os valores digitados foram:")
+print(f"primeiro valor:{valor1}")
+print(f"segundo valor: {valor2}")
+print(f"Resultado = {soma}")
