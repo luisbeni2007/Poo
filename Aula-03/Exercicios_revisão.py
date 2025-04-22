62ff6e00d5b13ecd13d8d2047934b19ae47e746b
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-04-22 15:13:45 +0000

    Praticando Pt.1

---
+++ Aula-03/Exercicios_revisão.py
@@ -0,0 +1,15 @@
+def comparar_números():
+
+    print("Digite dois valores inteiros")
+    num1 = int(input())
+    num2 = int(input())
+
+    if num1 > num2:
+        print(f"maior = {num1}")
+    elif num2  > num1:
+        print(F"maior = {num2}")
+    else:
+        print("Números iguais")
+
+        if __name__ == "__main__":
+            comparar_números()
