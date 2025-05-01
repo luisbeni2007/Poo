009699cf768ee374427cc8761331c09676951880
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-05-01 21:37:34 +0000

    Praticando_Pt.2 !

--- Aula-03/Exercicios_Revisão-2.py
+++ Aula-03/Exercicios_Revisão-2.py
@@ -5,3 +5,13 @@ def calcular_media_e_comparar():
         números.append(int(input()))
         media = sum (números) / len (números)
         print(f"Média= {int(media)}")
+
+    for num in números:
+        if num < media:
+            print(num)
+            print("Números menores que a media ")
+            
+            for num in números:
+                if num >= media:
+                    print(num)
+                    print("Números maiores  ou iguais a média ")
