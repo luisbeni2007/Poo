089d74936087316f03392b33d170e8e5f3647a36
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-05-01 22:20:41 +0000

    Praticando_Pt.3

---
+++ Aula-03/Exercicios_Revisão-3.py
@@ -0,0 +1,15 @@
+def calcular_somas():
+    print("Digite os quatro valores inteiros")
+    soma_pares = 0
+    soma_impares = 0
+
+    for i in range(4):
+        numero = int(input())
+
+        if numero % 2 == 0:
+            soma_pares += numero
+        else:
+            soma_impares += numero
+
+            print(f"Soma dos pares = {soma_pares}")
+            print(f"Soma dos impares = {soma_impares}")
