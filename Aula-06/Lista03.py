0f4f332a72fbed3025fb26f59591c9e9e0b0d28c
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-05-08 13:24:39 +0000

    pratiicando pra prova pt3

---
+++ Aula-06/Lista03.py
@@ -0,0 +1,5 @@
+class contabancaria:
+    def __init__(self,nome_titular, numero_conta, saldo ):
+        self.nome_titular = nome_titular
+        self.numero_conta = numero_conta
+        self.saldo = saldo
