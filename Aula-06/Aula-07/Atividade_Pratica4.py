7a863803fb1cf270baac134c5c0affa535d82e9f
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-06-09 14:44:55 +0000

    Revisão Para Prova Dia 10(amanhã)

--- Aula-06/Aula-07/Atividade_Pratica4.py
+++
@@ -1,8 +0,0 @@
-def calcular_valor_inteiro(self,):
-    dia = self.__dia
-    horario = self.horario
-
-    if dia == "quarta":
-        return 8.00
-
-    
--- Aula-09/Lista De Revisão04.py
+++ Aula-09/Lista De Revisão04.py
@@ -14,13 +14,33 @@ class viagem:
         return self.__litros
     
     def set_destino(self,destino):
-        self.destino =  destino
+        self.__destino =  destino
 
     def set_distancia(self,distancia):
-        self.distancia = distancia
+        self.__distancia = distancia
 
     def set_litros(self,litros):
-        self.litros = litros
+        self.__litros = litros
     
     def consumo(self):
         return self.__distancia / self.__litros
+    
+class ViagemUI:
+    @staticmethod
+    def main():
+        op = 0
+        while op != 2:
+            op = ViagemUI.menu()
+            if op == 1:
+                ViagemUI.calculo()
+    @staticmethod
+    def menu():
+        return int(input("1-calcular, 2-fim:"))
+    @staticmethod
+    def calculo():
+        destino = input("Informe o destino:")
+        distancia = float(input("distancia:"))
+        litros = float(input("litros:"))
+        X = viagem(destino,distancia,litros)
+        print(f"o consumo foi de {X.consumo():.1f}km/l")
+ViagemUI.main()
