db493d42695855b918fa731b73e813de8eb7fea1
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-06-24 02:57:54 +0000

    Prova hoje 24/07

--- Aula-09/Lista De Revisão05.py
+++ Aula-09/Lista De Revisão05.py
@@ -12,3 +12,20 @@ class frete:
     
     def get_peso(self):
         return self.__peso()
+    
+    def set_produto(self,produto):
+        self.__produto = produto
+
+    def set_distancia(self,distancia):
+        if isinstance(distancia, (int, float)) and distancia > 0:
+         self.__distancia = distancia
+        else:
+            raise ValueError("Distância deve ser um número positivo")
+
+    def set_peso(self, peso):
+         if isinstance(peso, (int, float)) and peso > 0:
+          self.__peso = peso
+
+    def preço(self):
+        return (self .__distancia * self .__peso) / 100
+        
